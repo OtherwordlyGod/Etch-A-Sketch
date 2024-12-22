aeed9bafc4d34a783f8bef5b7ae10f5aac45cc37
OtherwordlyGod <otherwordlygod@gmail.com>
2024-12-22 13:18:45 -0500

    finished base project

---
+++ main.js
@@ -0,0 +1,65 @@
+let canvasSize = 16;
+
+const container = document.getElementById("container");
+
+function generateCanvas(size) {
+   for (let i = 0; i < size * size; i ++) {
+       const square = document.createElement("div");
+       square.classList.add("grid-item");
+       square.style.width = `${500 / size}px`;
+       square.style.height = `${500 / size}px`;
+       container.appendChild(square);
+   };
+
+let mouseDown = false;
+   container.addEventListener("mousedown", () => {
+      mouseDown = true;
+   });
+
+   container.addEventListener("mouseup", () => {
+      mouseDown = false;
+   });
+
+ container.addEventListener("mousemove", (event) => {
+    if (mouseDown === true && event.target.classList.contains("grid-item")) {
+      const target = event.target;
+      const currentColor = window.getComputedStyle(target).backgroundColor;
+      
+      if (currentColor === "rgba(0, 0, 0, 0)") {
+        event.target.style.backgroundColor = randomRGB();
+      } 
+    };
+  });
+};
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  let input = document.getElementById("canvas-size")
+  redrawCanvas(input.value);
+})
+
+function redrawCanvas(userInput) {
+  let child = container.lastElementChild;
+  
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  };
+  
+  generateCanvas(userInput);
+}
+
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+  
+
+generateCanvas(canvasSize);
+
+
